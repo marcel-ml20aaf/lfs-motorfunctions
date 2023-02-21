96b9690835a396aa900d68436ac63de39b53e10b
marcel-ml20aaf <125359855+marcel-ml20aaf@users.noreply.github.com>
2023-02-21 10:50:55 +0000

    added delayto hard turn

--- main.ts
+++ main.ts
@@ -1,24 +1,26 @@
 let LFSL2 = 0
 let LFSR2 = 0
 function soft_left () {
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 25)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 80)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 45)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 90)
 }
 function all_ahead () {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 90)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 90)
 }
 function hard_right () {
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 80)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 25)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 90)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 15)
+    basic.pause(100)
 }
 function soft_right () {
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 80)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 25)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 90)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 45)
 }
 function hard_left () {
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 25)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 80)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 15)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 90)
+    basic.pause(100)
 }
 basic.forever(function () {
     LFSL2 = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
